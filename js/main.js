4c4934c7122b38de063f9e0b40a331727845ef5c
Михаил <beatbox787@gmail.com>
2017-04-22 18:57:46 +0700

    no message

---
+++ js/main.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){	
+    function resize(){
+       if( typeof( window.innerWidth ) == 'number' ) {
+            myWidth = window.innerWidth;
+            myHeight = window.innerHeight;
+        } else if( document.documentElement && ( document.documentElement.clientWidth || 
+        document.documentElement.clientHeight ) ) {
+            myWidth = document.documentElement.clientWidth;
+            myHeight = document.documentElement.clientHeight;
+        } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+            myWidth = document.body.clientWidth;
+            myHeight = document.body.clientHeight;
+        }
+    }
+    $(window).resize(resize);
+    resize();
+
+    $.fn.placeholder = function() {
+        if(typeof document.createElement("input").placeholder == 'undefined') {
+            $('[placeholder]').focus(function() {
+                var input = $(this);
+                if (input.val() == input.attr('placeholder')) {
+                    input.val('');
+                    input.removeClass('placeholder');
+                }
+            }).blur(function() {
+                var input = $(this);
+                if (input.val() == '' || input.val() == input.attr('placeholder')) {
+                    input.addClass('placeholder');
+                    input.val(input.attr('placeholder'));
+                }
+            }).blur().parents('form').submit(function() {
+                $(this).find('[placeholder]').each(function() {
+                    var input = $(this);
+                    if (input.val() == input.attr('placeholder')) {
+                        input.val('');
+                    }
+                });
+            });
+        }
+    }
+    $.fn.placeholder();
+    
+    $('.b-main-slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+
+    $('.b-product-slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+
+    $('.b-partners-slider').slick({
+        prevArrow: '<button type="button" class="slick-prev slick-button icon-arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next slick-button icon-arrow-right"></button>'
+    });
+
+	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
+ //    var myOptions = {
+ //        zoom: 16,
+ //        center: myPlace,
+ //        mapTypeId: google.maps.MapTypeId.ROADMAP,
+ //        disableDefaultUI: true,
+ //        scrollwheel: false,
+ //        zoomControl: true
+ //    }
+ //    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+
+ //    var marker = new google.maps.Marker({
+	//     position: myPlace,
+	//     map: map,
+	//     title: "Ярмарка вакансий и стажировок"
+	// });
+
+    //  var options = {
+    //     $AutoPlay: true,                                
+    //     $SlideDuration: 500,                            
+
+    //     $BulletNavigatorOptions: {                      
+    //         $Class: $JssorBulletNavigator$,             
+    //         $ChanceToShow: 2,                           
+    //         $AutoCenter: 1,                            
+    //         $Steps: 1,                                  
+    //         $Lanes: 1,                                  
+    //         $SpacingX: 10,                              
+    //         $SpacingY: 10,                              
+    //         $Orientation: 1                             
+    //     }
+    // };
+
+    // var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+});
